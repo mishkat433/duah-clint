e36ef15f865a111fff75fbaa147cb0c4c5f75354
Mishkat433 <mishkat433@gmail.com>
2024-03-29 23:52:51 +0600

    navbar error found

--- Components/Navbar/Sidebar.jsx
+++ Components/Navbar/Sidebar.jsx
@@ -7,19 +7,18 @@ import { AuthContex } from "../../context.jsx/AuthStorage";
 
 const Sidebar = () => {
 
-    // const { setSelectDuah } = useContext(AuthContex)
+    const { setSelectDuah } = useContext(AuthContex)
 
 
     return (
 
         <div className="text-white px-2 ml-4 py-4 z-0 border-white ">
             <ul className="space-y-2 text-sm">
-                {/* {
+                {
                     duaDatas?.map((data, i) => (
                         <li key={i} className="flex items-center gap-3 text-secondary font-semibold" ><GiBrightExplosion className="" /> <button onClick={() => setSelectDuah(data.title)} >{data.title}</button> </li>
                     ))
-                } */}
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur ad ducimus odit, illo veritatis harum ipsum architecto nobis rerum commodi reiciendis modi, tempora consequuntur ab unde impedit beata</p>
+                }
             </ul>
         </div>
 
